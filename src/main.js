fbe00e902306620ca1d4ead512efc33ead26fdf1
najiib11 <muhammadnajibzr110@gmail.com>
2024-10-18 20:04:34 +0700

    Menambahkan tailwindcss

--- src/main.js
+++ src/main.js
@@ -10,13 +10,13 @@ btnTambah.onclick = (e) => {
    }
    let task = document.createElement("div");
    task.id = `${Date.now()}`;
-   task.className = `...`;
+   task.className = `flex justify-between items-center bg-white shadow-md p-4 rounded-md`;
    task.innerHTML = `
-         <p class="...">${title}</p>
+         <p class="text-gray-700">${title}</p>
          <input type="button" id="btn-selesai" value="SELESAI" data-task="${task.id}"
-                class="...">
+                class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-md cursor-pointer">
          <input type="button" id="btn-hapus" value="HAPUS" data-task="${task.id}"
-                class="...">
+                class="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md cursor-pointer">
    `;
    output.appendChild(task);
    input.value = "";
